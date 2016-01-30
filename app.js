d6f1223bf9567af05117d3664d51c23c97fd8212
andrei <andrei.fzto@gmail.com>
2016-01-30 20:35:06 +0300

    wihout ()

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const port = process.env.PORT || 3000
 
 http.createServer((req,res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' })       
+
   res.end(JSON.stringify({
     IP: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     language: req.headers['accept-language'].split(',')[0],
-    software: req.headers['user-agent'].match(/(\(.+?\))/)[0]
+    software: req.headers['user-agent'].match(/\(.+?\)/)[1]
   }))
 }).listen(port)
