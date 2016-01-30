5fc008b70a8bdfbbbb62c1988e8a7fa908d7d7f9
andrei <andrei.fzto@gmail.com>
2016-01-30 20:25:12 +0300

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const http = require('http')
+const port = process.env.PORT || 3000
+// const hostname = process.env.IP || 'localhost'
+
+http.createServer((req,res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' })       
+  res.end(JSON.stringify({
+    IP: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    language: req.headers['accept-language'].split(',')[0],
+    software: req.headers['user-agent'].match(/(\(.+?\))/)[0]
+  }))
+}).listen(port)
