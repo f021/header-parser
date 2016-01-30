2b88b42a27891d233c69a596201bd0df786e760a
andrei <andrei.fzto@gmail.com>
2016-01-30 20:37:38 +0300

    fixed

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ http.createServer((req,res) => {
   res.end(JSON.stringify({
     IP: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     language: req.headers['accept-language'].split(',')[0],
-    software: req.headers['user-agent'].match(/\(.+?\)/)[1]
+    software: req.headers['user-agent'].match(/\((.+?)\)/)[1]
   }))
 }).listen(port)
